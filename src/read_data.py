efa4ec39d89d1504896e2140355658704acb9eeb
ShaziaSulemane <shazia.sulemane@gmail.com>
2022-12-04 16:05:10 +0000

    Added find_floor, draw_floor and measure lenght

--- src/read_data.py
+++ src/read_data.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 import tensorflow
 import tensorflow as tf
@@ -30,20 +31,60 @@ def read_json(path):
             points.append([shap_att['shape_attributes']['cx'], shap_att['shape_attributes']['cy']])
         dict[data[frame]['filename']] = points
         print("name:" + data[frame]['filename'] + " points: " + str(points))
-
     return dict
 
 
 def find_floor(dict):
-    for value in dict.values():
+    ymax = 0
+    for name in dict.keys():
+        value = dict.get(name)
+        for coord in value:
+            if ymax < coord[1]:
+                ymax = coord[1]
+        dict[name] = ymax
+    return dict
+
+
+def draw_floor(dict, img_path, floor_thickness=9, floor_color=(0, 255, 0)):
+    for name in dict.keys():
+        path = img_path + name
+        img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+        h, w = img.shape
+        init_coord = [0, dict.get(name)]
+        end_coord = [w, dict.get(name)]
+        cv.line(img, init_coord, end_coord, floor_color, floor_thickness)
+        cv.imshow("image", img)
+        cv.waitKey(0)
+
+
+def measure_lenght(dict, points_to_measure, pixel_irl=None):
+    for name in dict.keys():
+        value = dict.get(name)
+        dist = []
+        for p in points_to_measure:
+            if value:
+                try:
+                    d = math.dist(value[p[0]], value[p[1]])
+                    if pixel_irl:
+                        d = d * pixel_irl
+                    dist.append(d)
+                    dict[name] = dist
+                except:
+                    print("Exception Occured")
+                print("Distance: " + str(dist))
+
+
 
 
 def main():
     video_folder = "/home/shazia/PycharmProjects/a22207399/videos/VID_20210625_100510.mp4"
-    dataset_folder = "/home/shazia/PycharmProjects/a22207399/dataset"
+    dataset_folder = "/home/shazia/PycharmProjects/a22207399/dataset/"
     json_notations = ""
     d = read_json("/home/shazia/PycharmProjects/a22207399/dataset/via_project_6Nov2022_10h31m_json.json")
-    find_floor(d)
+    # floors = find_floor(d)
+    # draw_floor(floors, dataset_folder)
+    measure_lenght(d, [[0, 1], [1, 2]])
+
 
 if __name__ == "__main__":
     main()
