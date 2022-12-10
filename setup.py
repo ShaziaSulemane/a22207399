5324616137f38ba787932b21e17d9a2008ec8e88
ShaziaSulemane <shazia.sulemane@gmail.com>
2022-12-10 16:57:00 +0000

    added setup.py for pip creation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='a22207399',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='shazia',
+    author_email='shazia.sulemane@gmail.com',
+    description=''
+)
